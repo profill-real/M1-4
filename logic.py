e18814e910e166ec07da95c6ad693b845517f4e9
profill-real <dima.2018.sh@gmail.com>
2026-09-06 14:50:25 +0500

    test4

--- logic.py
+++ logic.py
@@ -11,12 +11,19 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.height = self.get_height()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
     def get_img(self):
-        pass
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['official-artwork']['front-default'])
+        else:
+            return None
     
     # Метод для получения имени покемона через API
     def get_name(self):
@@ -27,11 +34,19 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pikachu"
-
+        
+    def get_height(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['height'])
+        else:
+            return 0
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покеомона: {self.name}. Высота покеомона: {self.height}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
